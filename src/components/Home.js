72e4017c71cae15ccec3b74485bfb78cadc31814
michaelab24 <michaelbarnes459@yahoo.com>
2021-09-04 22:18:12 -0500

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React from "react";
+import image from "../mtns.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="beautiful mountain" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justfy-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="cursive home-name">Hello! I'm Michael</h1>
+            </section>
+        </main>
+    )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Post() {
-    return <h1>Post Page!</h1>
+    return 
 }
